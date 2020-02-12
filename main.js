e50bedb757c9a295ca5421cb105bca3019d4d21a
Max Lemieux <max.lemieux@gmail.com>
2020-02-11 18:16:23 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Define different views for the app
+var views = ["intro", "quiz", "completed", "highscores"];
+// Set initial view to intro
+var currentView = views[0];
+
+// Create top bar with high scores link on left, timer on right
+
+// Intro text with start button
